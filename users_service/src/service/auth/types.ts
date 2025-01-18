35e11581aa119aa92f19424a472991e40707ac11
WagnerAlvesMelo <walves13@outlook.com>
2025-01-17 22:01:57 -0300

    chore: add users submodule

--- users_service/src/service/auth/types.ts
+++
@@ -1,4 +0,0 @@
-export type AuthParams = {
-  email: string;
-  password: string;
-};
--- users_service/src/service/role/role.spec.ts
+++
@@ -1,3 +0,0 @@
-describe('[Service] - role', () => {
-  it('should get a role by description', async () => {});
-});
